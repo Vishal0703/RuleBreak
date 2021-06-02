c11ff3955700a7f89fe093c92a8fc55fa6db3cdf
Vishal Gupta <vishal7031311@gmail.com>
2021-06-02 18:37:48 +0530

    Rule Trigger on Rule Breaking added

--- Assets/Scripts/RuleDamage.cs
+++ Assets/Scripts/RuleDamage.cs
@@ -7,9 +7,16 @@ public class RuleDamage : Damage
 {
     [SerializeField] float cameraShakeAmplitude = 5f;
     [SerializeField] float cameraShakeTime = 0.1f;
+    [SerializeField] GameEvent ruleBreakEvent;
     public override void TakeDamage()
     {
         base.TakeDamage();
         CinemachineShake.cm.ShakeCamera(cameraShakeAmplitude, cameraShakeTime);
+    }
+
+    public override void Die(bool destroyGameObject = true)
+    {
+        ruleBreakEvent.Raise();
+        base.Die(destroyGameObject);
     }
 }
